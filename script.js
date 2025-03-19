f99ae76d7cdf915dea09e9b00e167a334da0e683
Lucie Philippon <lucie.philippon@proton.me>
2025-03-19 15:46:13 +0100

    fin jour 1

--- script.js
+++ script.js
@@ -28,3 +28,35 @@ const total = montantHT * (1 + tauxTVA) * nombreArticles;
 console.log(`Le montant TTC est ${total} €`);
 
 // 2
+
+let a = 1;
+let b = 2;
+
+console.log({ a, b });
+
+// Échanger les valeurs de a et b avec une affectation multiple
+[b, a] = [a, b];
+
+console.log({ a, b });
+
+// Échanger sans utiliser d’affectation
+const c = a;
+a = b;
+b = c;
+
+console.log({ a, b });
+
+// Échanger avec (+) et (-)
+// a = 1, b = 2
+a = a + b; // a = 3
+b = a - b; // b = 1
+a = a - b; // a = 2
+
+console.log({ a, b });
+
+// 3
+let [x, y, z] = [1, 2, 3];
+console.log({ x, y, z });
+
+[x, y, z] = [y, z, x];
+console.log({ x, y, z });
