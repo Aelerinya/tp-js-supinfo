8b9846f3765ca90d32e0feda1365e2e6cf220513
Lucie Philippon <lucie.philippon@proton.me>
2025-03-21 10:56:15 +0100

    Template jour 3 matin

---
+++ Jour_3_matin/script.js
@@ -0,0 +1,7 @@
+/**
+ * TP — Manipulation du DOM en JavaScript
+ * 1WEBD
+ */
+
+// Le bouton de changement de thème a l'id #theme-toggle
+// Pour passer au mode sombre, il faut ajouter la classe .dark-theme à l'élément body
