22de13706494c9adbcc7c22b13961c8658ae4ff7
Lucie Philippon <lucie.philippon@proton.me>
2025-03-21 13:08:52 +0100

    Correction Jour 3 Matin

--- Jour_3_matin/script.js
+++ Jour_3_matin/script.js
@@ -5,3 +5,14 @@
 
 // Le bouton de changement de thème a l'id #theme-toggle
 // Pour passer au mode sombre, il faut ajouter la classe .dark-theme à l'élément body
+
+const themeToggleButton = document.getElementById("theme-toggle")
+const body = document.querySelector("body")
+
+themeToggleButton.addEventListener("click", function (_e) {
+    if (body.classList.contains("dark-theme")) {
+        body.classList.remove("dark-theme")
+    } else {
+        body.classList.add("dark-theme")
+    }
+})
